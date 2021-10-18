429244bc7278f99ec01b6660b9e6971f0e8f30b5
Steven Booth <sbb237@cornell.edu>
2021-10-18 00:50:55 -0400

    Saving before checkout to network-fixes

--- src/Network.mli
+++ src/Network.mli
@@ -1,79 +1,77 @@
-(**Representation of static social network. 
+(**Representation of static social network.
 
-  This module handles the data stored in the file that stores a social network.
-  It includes the data of the people and their interactions. It handles loading
-  of two csv files (attribute list and edge list) that include the initial 
-  conditions and the parameters of the network, as well as querying the data. 
-  The attribute list csv file will contain the individual parameters of the 
-  people in the network. 
-  The edge list csv file will contain each interaction and their respective 
-  parameters.*)
+   This module handles the data stored in the file that stores a social
+   network. It includes the data of the people and their interactions.
+   It handles loading of two csv files (attribute list and edge list)
+   that include the initial conditions and the parameters of the
+   network, as well as querying the data. The attribute list csv file
+   will contain the individual parameters of the people in the network.
+   The edge list csv file will contain each interaction and their
+   respective parameters.*)
 
-  type t 
-  (** The abstract type of the values representing infection network*)
+type t
+(** The abstract type of the values representing infection network*)
 
-  type person_id = int
-  (** The type of person identifiers*)
+type person_id = int
+(** The type of person identifiers*)
 
-  type position = int list
-  (** The type of the position in the positive x-y plane. 
-  The first value of the list is the x value, the second is the y value.
-  Precondition: position has exactly two elements *)
+type position = int list
+(** The type of the position in the positive x-y plane. The first value
+    of the list is the x value, the second is the y value. Precondition:
+    position has exactly two elements *)
 
-type infected = 
-| Infected
-| Not_infected
-(**The type of the state of infection of a person*)
+type infected =
+  | Infected
+  | Not_infected  (**The type of the state of infection of a person*)
 
-
-type edge_info = {distance : float;
-                    risk : string} 
+type edge_info = {
+  distance : float;
+  risk : string;
+}
 (** The type of edge information.*)
 
-
-type attr = {infected : infected ;
-                    mask : string ;
-                    immunity : float;
-                    position : position}
+type attr = {
+  infected : infected;
+  mask : string;
+  immunity : float;
+  position : position;
+}
 (** The type of individual attributes of person [id]*)
 
 exception UnknownPerson of person_id
-  (** Raised when [person_id] is not in the network*)
+(** Raised when [person_id] is not in the network*)
 
-  exception UnknownEdge of person_id * person_id
+exception UnknownEdge of person_id * person_id
 (** Raised when a non-existing edge is encountered*)
 
-  exception InvalidJSON 
-  (** Raised when the input JSON has an invalid form*)
+exception InvalidJSON
+(** Raised when the input JSON has an invalid form*)
 
-  exception InvalidPosition of position
-  (** Raised when an illegal position is encountered*)
+exception InvalidPosition of position
+(** Raised when an illegal position is encountered*)
 
-  val from_json : Yojson.Basic.t -> t
-  (** [from_json j] is the network that [j] represents. Requires: [j] is
-    a valid JSON network representation. *)
+val from_json : Yojson.Basic.t -> t
+(** [from_json j] is the network that [j] represents. Requires: [j] is a
+    valid JSON network representation. *)
 
-  val head : t -> person_id
-  (**[head net] is the first person in the network (as specified in csv)*)
+val head : t -> person_id
+(**[head net] is the first person in the network (as specified in csv)*)
 
-  val neighbors : t -> person_id -> person_id list
-  (**[edges net p] is a set-like list of all the people who [p] has an 
-  interaction with in network [net]. Raises [UnknownPerson p] if the [p]
-  is not a person in [net]*)
+val neighbors : t -> person_id -> person_id list
+(**[edges net p] is a set-like list of all the people who [p] has an
+   interaction with in network [net]. Raises [UnknownPerson p] if the
+   [p] is not a person in [net]*)
 
-  val get_position : t -> person_id -> position
-  (** [get_position net p] is the position of person [p] 
-  in network [net]. Raises [UnknownPerson p] if person [p] 
-  does not exist in [net]*)
-  
-  val get_attributes : t -> person_id -> attr
-  (** attributes net p] is the attributes of person [p]
-  in network [net]. Raises [UnknownPerson p] if person [p] 
-   does not exist in [net]*)
+val get_position : t -> person_id -> position
+(** [get_position net p] is the position of person [p] in network [net].
+    Raises [UnknownPerson p] if person [p] does not exist in [net]*)
 
-  val edge_info : t -> person_id -> person_id -> edge_info
-  (** [get_edge_info net p1 p2] is the edge information of the edge
-  between person [p1] and person [p2].
-  Raised [UnknownPerson p1] if p1 does not exist in [net].
-  Raises [UnknownEdge (p1,p2)] if an edge between [p1] and [p2] does 
-  not exist.*)
+val get_attributes : t -> person_id -> attr
+(** attributes net p] is the attributes of person [p] in network [net].
+    Raises [UnknownPerson p] if person [p] does not exist in [net]*)
+
+val edge_info : t -> person_id -> person_id -> edge_info
+(** [get_edge_info net p1 p2] is the edge information of the edge
+    between person [p1] and person [p2]. Raised [UnknownPerson p1] if p1
+    does not exist in [net]. Raises [UnknownEdge (p1,p2)] if an edge
+    between [p1] and [p2] does not exist.*)
--- src/state.ml
+++ src/state.ml
@@ -1,3 +1,27 @@
+module PersonKey = struct
+  type t = Network.person_id
+  let compare = Stdlib.compare
+end
+
+
+module NodeMap = Map.Make(PersonKey)
+
+(** [VisitedTracker] implements a logger for tracking which nodes have
+    been visited during a graph traversal*)
+class visitedTracker =
+    object (self)
+      val mutable map = NodeMap.empty (* can i make this private?*)
+      (** [add_visit n] updates tracker with the fact that node [n] has been visited *)
+      method add_visit n =                          
+        map <- NodeMap.add n true map
+      (** [was_visited n] is if node [n] has been visited before*)
+      method was_visited (n: Network.person_id) : bool =                               
+        NodeMap.mem n map
+      (** [clear] deletes all entries from the tracker*)
+      method clear =
+        map <- NodeMap.empty
+    end;;
+
 (** [infection_prob node neighbor] is the likelihood of [node] being
     infected by [neighbor]*)
 let infection_prob node neighbor = 1.
@@ -35,27 +59,6 @@ let rec generate_updates state tracker n = generate_tr [] state tracker n
 let rec apply_changes state changes = failwith "Failure: Unimplemented"
 
 let update_state state =
-  state |> Graph.head |> generate_updates |> apply_changes state
+  state
+  (* state |> Network.head |> generate_updates |> apply_changes state *)
 
-module PersonKey = struct
-  type t = Network.person_attr
-  let compare (p1: Network.person_attr) (p2: Network.person_attr) = Stdlib.compare p1.id p2.id
-end
-
-module NodeMap = Map.Make(PersonKey)
-
-(** [VisitedTracker] implements a logger for tracking which nodes have
-    been visited during a graph traversal*)
-class visitedTracker =
-    object (self)
-      val mutable map = NodeMap.empty (* can i make this private?*)
-      (** [add_visit n] updates tracker with the fact that node [n] has been visited *)
-      method add_visit (n :  =                           
-        map <- NodeMap.add n true map
-      (** [was_visited n] is if node [n] has been visited before*)
-      method was_visited (n: Network.person_attr) :bool =                               
-        list.mem n map
-      (** [clear] deletes all entries from the tracker*)
-      method clear =
-        map <- NodeMap.empty
-    end;;
