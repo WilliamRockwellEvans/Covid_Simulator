e74a9a6cc99164b1f092189f24371a23ba2596c4
Steven Booth <sbb237@cornell.edu>
2021-10-17 22:07:13 -0400

    Implements tracker 	- all muddled and wrong, waiting on network to be able to finish and test functions

--- src/state.ml
+++ src/state.ml
@@ -1,44 +1,3 @@
-type wellbeing =
-  | Healthy
-  | Sick
-
-type node = {
-  id : string;
-  neighbors : node list;
-  status : wellbeing;
-  mutable processed : bool;
-}
-
-module Node = struct
-  type t = node
-
-  let compare a b = Stdlib.compare a.id b.id
-end
-
-module NodeMap = Map.Make (Node)
-
-module Graph = struct
-  let t =
-    NodeMap.(
-      empty
-      |> add
-           {
-             id = "hi";
-             neighbors = [];
-             status = Healthy;
-             processed = false;
-           }
-           0)
-
-  let head state =
-    { id = "hi"; neighbors = []; status = Healthy; processed = false }
-
-  let find_node n state =
-    if NodeMap.mem n state then Some (NodeMap.find n state) else None
-
-  let list_nodes state = []
-end
-
 (** [infection_prob node neighbor] is the likelihood of [node] being
     infected by [neighbor]*)
 let infection_prob node neighbor = 1.
@@ -47,41 +6,56 @@ let infection_prob node neighbor = 1.
     [prob] *)
 let rand_bool prob : bool = prob <= Random.float 1.
 
-let not_visited n = n.processed
-
-(** [process_node n] is whether [n] gets infected by its neighbors *)
-let process_node n =
+(** [process_node state n] is whether node [n] in state [state] gets infected by its neighbors *)
+let process_node state n =
   let rec lst_itr acc = function
     | h :: t -> lst_itr (infection_prob n h :: acc) t
     | [] -> acc
   in
-  lst_itr [] n.neighbors |> List.map rand_bool
+  lst_itr [] (Network.neighbors state n) |> List.map rand_bool
   |> List.exists (fun x -> x)
 
-let rec generate_updates acc n =
-  n.processed <- true;
-  (* Sets this node to having been visited*)
-  let rec lst_itr unvisited_neighbors =
-    match unvisited_neighbors with
-    | [] -> []
-    | h :: t -> generate_updates [] h @ lst_itr t
-  in
-  if process_node n then
-    n :: lst_itr (n.neighbors |> List.filter not_visited)
-  else lst_itr (n.neighbors |> List.filter not_visited)
+  let not_yet_visited tracker n = not (tracker#was_visited n)
 
-let rec apply_changes state changes = failwith "Failure: unimplemented"
+  let rec generate_tr acc state tracker n =  
+    tracker#add_visit n;
+    let rec lst_itr unvisited_neighbors =
+      match unvisited_neighbors with
+      | [] -> acc
+      | h :: t -> generate_tr (h @ lst_itr t) state tracker h
+    in let processed_neighbors = lst_itr ((Network.neighbors state n) |> List.filter (not_yet_visited tracker))
+    in if process_node state n then
+        n :: processed_neighbors
+      else processed_neighbors
+
+(** [generate_updates tracker n] is the list of updates to be applied to the graph*)
+let rec generate_updates state tracker n = generate_tr [] state tracker n
+
+(** [apply_changes state changes] parses through the graph and applies [changes]*)
+let rec apply_changes state changes = failwith "Failure: Unimplemented"
 
 let update_state state =
-  state |> Graph.head |> generate_updates [] |> apply_changes state
+  state |> Graph.head |> generate_updates |> apply_changes state
 
-(* module VisitedTracker : sig type t (** The abstract type for the
-   VisitedTracker*) val init : t (** [init] creates a new Tracker with
-   no logs*) val add_visit : Node.t -> t (** [add_visit node t] updates
-   tracker [t] with the fact that [node] has been visited *) val
-   was_visited : Node.t -> bool (** [was_visited node] is if node has
-   been visited before*) end = struct type t = node let init =
-   NodeMap.empty let add_visit node track= NodeMap.add node true track
-   let was_visited node track = NodeMap.mem node track end (**
-   [VisitedTracker] implements a logger for tracking which nodes have
-   been visited during a graph traversal*) *)
+module PersonKey = struct
+  type t = Network.person_attr
+  let compare (p1: Network.person_attr) (p2: Network.person_attr) = Stdlib.compare p1.id p2.id
+end
+
+module NodeMap = Map.Make(PersonKey)
+
+(** [VisitedTracker] implements a logger for tracking which nodes have
+    been visited during a graph traversal*)
+class visitedTracker =
+    object (self)
+      val mutable map = NodeMap.empty (* can i make this private?*)
+      (** [add_visit n] updates tracker with the fact that node [n] has been visited *)
+      method add_visit (n :  =                           
+        map <- NodeMap.add n true map
+      (** [was_visited n] is if node [n] has been visited before*)
+      method was_visited (n: Network.person_attr) :bool =                               
+        list.mem n map
+      (** [clear] deletes all entries from the tracker*)
+      method clear =
+        map <- NodeMap.empty
+    end;;
--- src/state.mli
+++ src/state.mli
@@ -1,12 +1,11 @@
 (** Infection network stepping
 
-    This module implements stepping from one infection state to the next 
+    This module implements stepping from one infection state to the next
     for the network *)
 
 type t
 (** The abstract type representing infection networks*)
 
-val update_state: t -> t
-(** [update_state state] is the infection network generated from [state] after 
-one time. Requires: [state] is a valid infection network*)
-
+val update_state : t -> t
+(** [update_state state] is the infection network generated from [state]
+    after one time. Requires: [state] is a valid infection network*)
