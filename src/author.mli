f4e248924659dc9f3857afdba3296abd90ada40f
Andy Schang <ams722@cornell.edu>
2021-10-20 22:07:04 -0400

    added author module

---
+++ src/author.mli
@@ -0,0 +1,6 @@
+(** CS 3110 Fall 2021 Final Project sprint 1 MS1
+
+    @authors Andy Schang (ams722) Will Evans (wre23) Steven Booth (sbb237) *)
+
+ val hours_worked : int
+ (** [hours_worked] is the number of hours you worked on this assignment. *)
